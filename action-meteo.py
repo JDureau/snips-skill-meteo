5f18f8f06aad7af9e08faf0b766b3e4922444b3e
Joseph Dureau <joseph.dureau@snips.ai>
2018-05-25 23:00:08 +0200

    fix

--- action-meteo.py
+++ action-meteo.py
@@ -89,7 +89,8 @@ def meteo_generale_callback(hermes, intentMessage):
     weather_forecast = get_weather_forecast({})
 
     response = (    "Il fait {0}. " 
-                    "La temperature max aujourd'hui est de {1}, minimum {2}.",
+                    "La temperature max aujourd'hui est de {1}, minimum {2}."
+        ).format(
             weather_forecast["temperature"], 
             weather_forecast["temperatureMax"], 
             weather_forecast["temperatureMin"]
