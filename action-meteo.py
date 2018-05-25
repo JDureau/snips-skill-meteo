469598cf7835ca5438f0dbab5c69bb8ca15a51e3
Joseph Dureau <joseph.dureau@snips.ai>
2018-05-25 22:22:28 +0200

    fix

--- action-meteo.py
+++ action-meteo.py
@@ -6,8 +6,6 @@ from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
 
-from snipsmopidy.snipsmopidy import SnipsMopidy
-
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
