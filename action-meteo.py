481c592cbbcbe42f74e9d06314959dbbadc15c06
Joseph Dureau <joseph.dureau@snips.ai>
2018-06-15 23:19:11 +0200

    add log

--- action-meteo.py
+++ action-meteo.py
@@ -84,7 +84,7 @@ def parse_open_weather_map_forecast_response(response, location, time):
     today = fromtimestamp(response["list"][0]["dt"]).day
     print()
     print(time)
-    print(time.from)
+    print(time)
     print(time.to)
     print(time.kind)
     print(time.type())
