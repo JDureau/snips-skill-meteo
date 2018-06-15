97fdccf910583809c16d42a71fa7c85435a54a61
Joseph Dureau <joseph.dureau@snips.ai>
2018-06-15 22:53:59 +0200

    print type

--- action-meteo.py
+++ action-meteo.py
@@ -84,6 +84,9 @@ def parse_open_weather_map_forecast_response(response, location, time):
     today = fromtimestamp(response["list"][0]["dt"]).day
     print()
     print(time)
+    print(time.type
+    print(time.type())
+    print()
     value = time.get("value", {})
     print()
     print(time)
